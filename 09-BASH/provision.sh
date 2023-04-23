913ab699f0ce273e1f91721ef45b22b388159cbe
buravtsovpavel <94462800+buravtsovpavel@users.noreply.github.com>
2023-04-23 20:44:02 +0500

    Update provision.sh

--- 09-BASH/provision.sh
+++ 09-BASH/provision.sh
@@ -4,6 +4,6 @@ yum install epel-release -y
 yum install mailx -y
 yum install  postfix -y
 sudo chmod a+x /vagrant/access-log-parse.sh
-sudo echo "* * * * * root /vagrant/access-log-parse.sh" > /etc/crontab
+sudo echo "0 * * * * root /vagrant/access-log-parse.sh" > /etc/crontab
 systemctl reload crond
 
